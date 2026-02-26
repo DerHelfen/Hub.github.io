2f1a617b30a16eca6bf3c359b1e90ac95ff4258c
Jan Helfen <jhelfen9616@cosmoconsult.com>
2026-02-26 21:38:46 +0100

    Fix icon path in index.html and update base URL in vite.config.js

--- hub/vite.config.js
+++ hub/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hub/",
+  base: "/Hub.github.io/",
   server: {
     port: 5173,
     open: true,
